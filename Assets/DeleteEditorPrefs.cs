7e32f3c92ba013a557b0544ce1b4f604444a24fe
Stanislav <s.tikhomirov@theilluminus.com>
2019-09-21 14:57:03 +0300

    Added "DeleteEditorPrefs"

---
+++ Assets/DeleteEditorPrefs.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DeleteEditorPrefs : MonoBehaviour
+{
+    [MenuItem("EditorPrefs/Clear all Editor Preferences")]
+    static void deleteAllExample()
+    {
+        if (EditorUtility.DisplayDialog("Delete all editor preferences.",
+            "Are you sure you want to delete all the editor preferences? " +
+            "This action cannot be undone.", "Yes", "No"))
+        {
+            Debug.Log("yes");
+            EditorPrefs.DeleteAll();
+        }
+    }
+}
